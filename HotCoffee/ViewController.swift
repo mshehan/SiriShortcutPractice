2092b3f213ae8dfec5fef5dbeb095a6680eea8e0
Matthew Shehan <matthew.shehan@macys.com>
2018-11-15 11:08:29 -0800

    Initial Commit

---
+++ HotCoffee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotCoffee
+//
+//  Created by Matthew Shehan on 11/15/18.
+//  Copyright © 2018 Macys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
